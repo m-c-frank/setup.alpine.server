ed83d7212c7e109d65788d21a011437c0c4e96b3
Martin Christoph Frank <martinchristophfrank@gmail.com>
2023-11-14 06:59:40 +0100

    moving to output dir

--- download-and-verify.sh
+++ download-and-verify.sh
@@ -1,43 +1,65 @@
 #!/bin/bash
 
-# URLs
-ISO_URL="https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-standard-3.18.4-x86_64.iso"
-SHA256_URL="https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-standard-3.18.4-x86_64.iso.sha256"
-GPG_URL="https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-standard-3.18.4-x86_64.iso.asc"
-GPG_KEY_URL="https://www.alpinelinux.org/keys/ncopa.asc"
+# Base URL
+BASE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/"
+KEY_URL="https://www.alpinelinux.org/keys/ncopa.asc"
+VERSION="3.18.4"
+ARCH="x86_64"
+ISO_NAME="alpine-standard-${VERSION}-${ARCH}.iso"
 
-# Download the ISO, SHA256, GPG files, and GPG Key
-echo "Downloading Alpine Linux ISO, SHA256 checksum, GPG signature, and GPG key..."
-curl -O "$ISO_URL"
-curl -O "$SHA256_URL"
-curl -O "$GPG_URL"
-curl -O "$GPG_KEY_URL"
+# Define the files and their corresponding URLs
+declare -A FILE_URLS=(
+  ["$ISO_NAME"]="${BASE_URL}${ISO_NAME}"
+  ["${ISO_NAME}.sha256"]="${BASE_URL}${ISO_NAME}.sha256"
+  ["${ISO_NAME}.asc"]="${BASE_URL}${ISO_NAME}.asc"
+  ["ncopa.asc"]="$KEY_URL"
+)
 
-# Import the GPG key
-echo "Importing GPG key..."
-gpg --import ncopa.asc
+PATH_OUT="./output"
 
-# Verify the SHA256 checksum
-echo "Verifying the SHA256 checksum..."
-sha256sum -c alpine-standard-3.18.4-x86_64.iso.sha256
+create_output_dir() {
+  mkdir -p "$PATH_OUT" && cd "$PATH_OUT" || exit 1
+}
 
-if [ $? -ne 0 ]; then
-    echo "Checksum verification failed!"
-    exit 1
-else
-    echo "Checksum verified successfully."
-fi
+download_file() {
+  local file=$1
+  local url=$2
+  echo "Downloading $file..."
+  curl -o "$file" "$url" || { echo "Failed to download $file"; exit 1; }
+}
 
-# Verify the GPG signature
-echo "Verifying the GPG signature..."
-gpg --verify alpine-standard-3.18.4-x86_64.iso.asc alpine-standard-3.18.4-x86_64.iso
+download_files() {
+  for file in "${!FILE_URLS[@]}"; do
+    download_file "$file" "${FILE_URLS[$file]}"
+  done
+}
 
-if [ $? -ne 0 ]; then
-    echo "GPG signature verification failed!"
-    exit 1
-else
-    echo "GPG signature verified successfully."
-fi
+import_gpg_key() {
+  echo "Importing GPG key..."
+  gpg --import "ncopa.asc" || { echo "GPG key import failed"; exit 1; }
+}
 
-echo "Download and verification completed successfully."
+verify_checksum() {
+  echo "Verifying the SHA256 checksum..."
+  sha256sum -c "${ISO_NAME}.sha256" || { echo "Checksum verification failed!"; exit 1; }
+  echo "Checksum verified successfully."
+}
+
+verify_gpg_signature() {
+  echo "Verifying the GPG signature..."
+  gpg --verify "${ISO_NAME}.asc" "${ISO_NAME}" || { echo "GPG signature verification failed!"; exit 1; }
+  echo "GPG signature verified successfully."
+}
+
+main() {
+  create_output_dir
+  echo "Downloading Alpine Linux ISO, SHA256 checksum, GPG signature, and GPG key..."
+  download_files
+  import_gpg_key
+  verify_checksum
+  verify_gpg_signature
+  echo "Download and verification completed successfully."
+}
+
+main
 
